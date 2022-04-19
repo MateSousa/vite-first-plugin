69cb50eaada6988e4bf9bd5baedf9d7ee3b9462f
Matheus Sousa <73663610+MateSousa@users.noreply.github.com>
2022-04-19 13:50:59 -0300

    chore:  init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <FirstPlugin msg="Hello from plugin"/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
